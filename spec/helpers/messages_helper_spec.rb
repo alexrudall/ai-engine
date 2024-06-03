5a1eec8898a308592ac7eaba6e66076f42be229b
Alex Rudall <alexrudall@gmail.com>
2024-06-03 16:18:14 +0100

    Fix spec

--- spec/helpers/messages_helper_spec.rb
+++ spec/helpers/messages_helper_spec.rb
@@ -4,11 +4,15 @@ RSpec.describe MessagesHelper, type: :helper do
   describe "#message_assistant_options" do
     let(:current_user) { create(:user) }
     let(:other_user) { create(:user) }
-    let!(:storyteller1) { create(:storyteller, user: current_user) }
-    let!(:storyteller2) { create(:storyteller, user: other_user) }
+    let(:storyteller1) { create(:storyteller, user: current_user) }
+    let(:storyteller2) { create(:storyteller, user: other_user) }
+    let(:assistants_service) { class_double(AI::Engine::OpenAI::Assistants::Create).as_stubbed_const }
 
     before do
+      allow(assistants_service).to receive(:call).and_return(Faker::Alphanumeric.alphanumeric(number: 10))
       allow(controller).to receive(:current_user).and_return(current_user)
+      storyteller1
+      storyteller2
     end
 
     it "returns options for the current user's storytellers" do
